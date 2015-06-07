a0c11e8d6bf3fad758177e4a83335c3c62929d00
tanghuihui <tanghuihui037@1caifu.com>
2015-06-07 21:57:21 +0800

    提交创建一个NodeJS + Express + Hogan模板的MVC站点

---
+++ hogan/routes/success.js
@@ -0,0 +1,23 @@
+/**
+ * Created by Administrator on 2015/6/7.
+ */
+var express = require('express');
+var router = express.Router();
+
+
+router.get('/', function(req, res, next) {
+    res.render('success', { title: 'Express'  ,username:new Date().getDate()});
+});
+
+
+router.post('/', function(req, res, next) {
+    if(req.body.username.replace(/(^\s*)|(\s*$)/g, "")=="" || req.body.password.replace(/(^\s*)|(\s*$)/g, "")=="" ){
+        res.redirect('/index');
+    }else{
+        res.render('success', { title: 'Express' ,username:req.body.username});
+    }
+
+});
+
+
+module.exports = router;
