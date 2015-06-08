c048c0f1757409ec3283295ff7601ad661bddf37
tanghuihui <tanghuihui037@1caifu.com>
2015-06-08 20:10:49 +0800

    提交作业二 NodeJS第三课作业

--- hogan/routes/success.js
+++ hogan/routes/success.js
@@ -3,7 +3,7 @@
  */
 var express = require('express');
 var router = express.Router();
-
+var Time=require('../Biz/Time');
 
 router.get('/', function(req, res, next) {
     res.render('success', { title: 'Express'  ,username:new Date().getDate()});
@@ -14,10 +14,18 @@ router.post('/', function(req, res, next) {
     if(req.body.username.replace(/(^\s*)|(\s*$)/g, "")=="" || req.body.password.replace(/(^\s*)|(\s*$)/g, "")=="" ){
         res.redirect('/');
     }else{
-        res.render('success', { title: 'Express' ,username:req.body.username});
+        console.log(Time.Time());
+        res.render('success', { title: 'Express' ,greeting:Time.Time(),username:req.body.username,hhlist:[
+
+            {picName:'哆啦A梦',picDesc:'第七公社 三口亲子装夏装2015新款 哆啦A梦全家装t恤衫短袖母女装 ',imgSrd:'/images/1.jpg'},
+            {picName:'哆啦A梦',picDesc:'第七公社 亲子装夏装2015款哆啦A梦全家装 韩版母女装短袖t恤童装 ',imgSrd:'/images/2.jpg'},
+            {picName:'哆啦A梦',picDesc:'第七公社 短袖T恤女夏装情侣装 2015新款体恤哆啦A梦宽松半袖t恤 ',imgSrd:'/images/3.jpg'},
+            {picName:'哆啦A梦',picDesc:'第七公社 哆啦A梦伴我同行T恤女夏装新款2015印花短袖t恤闺蜜装萌 ',imgSrd:'/images/4.jpg'},
+            {picName:'哆啦A梦',picDesc:'第七公社 扑倒蓝胖子哆啦a梦t恤情侣装夏装2015款短袖t恤女韩版潮 ',imgSrd:'/images/5.jpg'}
+        ]});
     }
 
-});
+})
 
 
 module.exports = router;
